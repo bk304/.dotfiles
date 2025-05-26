0009b1d04331556a100bc41f45b501c4d4e415da
bk304 <xnep1@hotmail.com>
2025-05-25 21:36:02 -0300

    25may

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,7 @@ zoxide init fish | source
 set PATH $PATH:/home/bruno/.cargo/bin
 set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
-eval (ssh-agent -c) > /dev/null
-ssh-add ~/.ssh/k304_rsa > /dev/null 2>&1
+eval (ssh-agent -c) >/dev/null
+ssh-add ~/.ssh/k304_rsa >/dev/null 2>&1
 
+alias h helix
